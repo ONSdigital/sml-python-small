62247aaa3b0a9e833ce02407a5cdd4fcafef8a03
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-06-21 10:40:38 +0100

    Testing for bugs

--- sml_small/totals_and_components/example.py
+++ sml_small/totals_and_components/example.py
@@ -477,5 +477,5 @@ A,202301,1625,632,732,99,162,TRUE,1625,202301,,11"""
 # You can run the functions invoke_process_in_memory_data_example or invoke_process_in_memory_data_example_2 below
 # invoke_process_with_local_csv()
 # invoke_process_with_in_memory_csv()
-# invoke_process_in_memory_data_example()
+invoke_process_in_memory_data_example()
 # invoke_process_in_memory_data_example_2()
