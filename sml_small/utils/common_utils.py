8cf0fa67f02bfc13ededede78cc0b1fb38ec4d50
jhmjam <james.morgan@ons.gov.uk>
2023-07-27 13:27:37 +0100

    added comments

--- sml_small/utils/common_utils.py
+++ sml_small/utils/common_utils.py
@@ -7,9 +7,18 @@ For Copyright information, please see LICENCE.
 from sml_small.utils.error_utils import get_params_is_not_a_number_error
 
 
+# This is a function to print a table.
+# An example input would look like the following:
+# print_table(
+#         "Name of your table",
+#         variable_name_1=variable_value_1,
+#         variable_name_2=variable_value_2,
+#         ...
+#         variable_name_n=variable_value_n,
+#     )
 def print_table(table_name: str, **kwargs):
     """
-    Prints the parsed attributes
+    Prints the parsed attributes.
 
     :param kwargs:
     :type kwargs: kwargs
@@ -17,13 +26,21 @@ def print_table(table_name: str, **kwargs):
     """
     # Print table of variable names and values
     print("\n")
+
     print(table_name)
+
     print("Variable Name   |   Value")
+
     print("----------------|---------")
+
     for var_name, var_value in kwargs.items():
         print(f"{var_name:<15}|   {var_value}")
 
 
+# This is a function to validate an input is a number.
+# Example of usage is as follows:
+# if validate_number("total", total) is True:
+#     then do something
 def validate_number(tag: str, value: str) -> bool:
     """
     This function will take a parsed tag and a value.
@@ -39,11 +56,15 @@ def validate_number(tag: str, value: str) -> bool:
     :return: Returns True when a value can be converted to float.
     :rtype: boolean
     """
+
     if not is_number(value):
         raise ValueError(get_params_is_not_a_number_error(tag))
+
     return True
 
 
+# This function is part of validate_number it will check if the value can
+# be converted to a float
 def is_number(value) -> bool:
     """
     This function attempts to convert an entered type into a float.
@@ -54,8 +75,11 @@ def is_number(value) -> bool:
     :return: This return a True boolean value if the value obtained can be converted to a float.
     :rtype: boolean to indicate if value is a number or not.
     """
+
     try:
         float(value)
+
     except Exception:
         return False
+
     return True
