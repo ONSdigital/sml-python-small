8584cdf797147e7f8f8b51a13b00a5085a02fcdb
jasonbellONS <jason.bell@ons.gov.uk>
2023-09-13 13:08:10 +0100

    Logging introduced, docstrings added

--- sml_small/editing/thousand_pounds/thousand_pounds.py
+++ sml_small/editing/thousand_pounds/thousand_pounds.py
@@ -1,9 +1,18 @@
 from dataclasses import dataclass
 from enum import Enum
 from typing import List, Optional, Tuple, Union
-
-from sml_small.utils.common_utils import validate_number
+from os import path
+import logging
+from sml_small.utils.common_utils import validate_number, log_table
 from sml_small.utils.error_utils import get_boundary_error, get_mandatory_param_error, get_one_of_params_mandatory_error
+
+
+# Pick up configuration for logging
+log_config_path = path.join(path.dirname(path.abspath(__file__)), "../../logging.conf")
+logging.config.fileConfig(log_config_path)
+
+# Create logger
+logger = logging.getLogger("thousandPounds")
 
 
 # --- Enum Definitions ---
@@ -53,22 +62,57 @@ class TPException(Exception):
 
 
 # --- Method Definitions ---
-def run(
-    principal_identifier: Optional[str],  # Unique identifer e.g. a question code - q500
-    principal_variable: float,  # Original response value provided for the 'current' period
+def thousand_pounds(
+    principal_identifier: Optional[str],  #
+    principal_variable: float,  #
     predictive: Optional[
         float
-    ],  # Value used for 'previous' response (Returned/Imputed/Constructed)
-    auxiliary: Optional[float],  # Calculated response for the 'previous' period
-    upper_limit: float,  # Upper bound of 'error ratio' threshold
-    lower_limit: float,  # Lower bound of 'error ratio' threshold
+    ],  #
+    auxiliary: Optional[float],  #
+    upper_limit: float,  #
+    lower_limit: float,  #
     target_variables: List[Target_variable],
 ) -> Thousands_output:
     """
     Calculates a pounds thousands error ratio and if the ration is between the bounds of the given limits will adjust
     the given principal variable and any linked variables by a factor of 1000.
+
+    :param principal_identifier: Unique identifier e.g. a question code - q500
+    :type principal_identifier: Optional[String]
+    :param principal_variable: Original response value provided for the 'current' period
+    :type principal_variable: float
+    :param predictive: Value used for 'previous' response (Returned/Imputed/Constructed)
+    :type predictive: Optional[float]
+    :param auxiliary: Calculated response for the 'previous' period
+    :type auxiliary: Optional[float]
+    :param upper_limit: Upper bound of 'error ratio' threshold
+    :param upper_limit: float
+    :param lower_limit: Lower bound of 'error ratio' threshold
+    :type lower_limit: float
+    :param target_variables: List of monetary variables that may be automatically corrected
+    :type target_variables: List[Target_variable]
+
+    :return: Thousands_output: An object containing the
+                            following attributes:
+            - principal_identifier: Unique identifer e.g. a question code - q500
+            - principal_original_value: Original provided value
+            - principal_final_value: Output value that may or may not be adjusted
+            - target_variables: Output linked values that may or may not be adjusted
+            - tpc_ratio: Ratio of the principal variable against good/predictive/aux response
+            - tpc_marker: C = Correction applied | N = No correction applied | E = Process failure
+
     """
 
+    log_table(
+        "Input Table Function",
+        principal_identifier=principal_identifier,
+        principal_variable=principal_variable,
+        predictive=predictive,
+        auxiliary=auxiliary,
+        upper_limit=upper_limit,
+        lower_limit=lower_limit,
+        target_variables=target_variables,
+    )
     error_ratio = None
     do_adjustment = False
     try:
@@ -103,6 +147,16 @@ def run(
         )
         target_variables_final = adjust_target_variables(
             do_adjustment, input_parameters[InputParameters.TARGET_VARIABLES.value]
+        )
+        log_table(
+            "Thousand Pounds Output",
+            principal_identifier=principal_identifier,
+            principal_variable=principal_variable,
+            predictive=predictive,
+            auxiliary=auxiliary,
+            upper_limit=upper_limit,
+            lower_limit=lower_limit,
+            target_variables=target_variables,
         )
 
         return Thousands_output(
@@ -158,7 +212,7 @@ def validate_input(
     :param upper_limit: Upper bound of 'error ratio' threshold
     :type upper_limit: float
     :param target_variables: List of monetary variables that may be automatically corrected
-    :type target_variables: List[Targer_variable]
+    :type target_variables: List[Target_variable]
 
     :return: predictive is returned as a converted float
     :rtype: float | None
@@ -207,12 +261,34 @@ def validate_input(
 
 
 def determine_tpc_marker(do_adjustment: bool) -> str:
+    """
+    Determine correction marker that should be output, "C" if values can be corrected, "N" if not.
+
+    :param do_adjustment: Marker for if principal variable falls within adjustable range
+    :type do_adjustment: bool
+
+    :return: "C" or "N"
+    :rtype: char
+    """
     return "C" if do_adjustment else "N"
 
 
 def determine_predictive_value(
     predictive: Optional[float], auxiliary: Optional[float]
 ) -> float:
+    """
+    Determine which value to use as the predictive value, predictive if input or auxiliary if no predictive is
+    available
+
+    :param predictive: Value used for 'previous' response (Returned/Imputed/Constructed)
+    :type predictive: Optional[float]
+    :param auxiliary: Calculated response for the 'previous' period
+    :type auxiliary: Optional[float]
+
+    :return: Either the input predictive or auxiliary value, which has been determined to used for the rest
+     of the method
+    :rtype: float
+    """
     if predictive or predictive == 0:
         return predictive
     if auxiliary:
@@ -220,6 +296,17 @@ def determine_predictive_value(
 
 
 def calculate_error_ratio(principal_variable: float, predictive_value: float) -> float:
+    """
+    Calculate the ratio for an acceptable error.
+
+    :param principal_variable: Original response value provided for the 'current' period
+    :type principal_variable: float
+    :param predictive_value: Either the input predictive or auxiliary value
+    :type predictive_value: float
+
+    :return: principal_variable / predictive_value, the calculated acceptable error ratio
+    :rtype: float
+    """
     return principal_variable / predictive_value  # predictive is already validated
 
 
@@ -227,6 +314,19 @@ def calculate_error_ratio(principal_variable: float, predictive_value: float) ->
 def is_within_threshold(
     error_ratio: float, lower_limit: float, upper_limit: float
 ) -> bool:
+    """
+    Determines if the error ratio is within the lower and upper limit input
+
+    :param error_ratio: Previously determined error ratio in "calculate_error_ratio"
+    :type error_ratio: float
+    :param lower_limit: Lower bound of 'error ratio' threshold
+    :type lower_limit: float
+    :param upper_limit: Upper bound of 'error ratio' threshold
+    :type upper_limit: float
+
+    :return: Boolean, True if error ratio is between lower and upper limits
+    :rtype: Bool
+    """
     if lower_limit < error_ratio < upper_limit:
         return True
     return False  # Outside the bounds of the threshold, do not adjust
@@ -234,6 +334,15 @@ def is_within_threshold(
 
 # Perform the calculation to adjust the response value
 def adjust_value(value: Optional[float]) -> Optional[float]:
+    """
+    Method to calculate the adjustment of the response value
+
+    :param value: response value to adjust
+    :type value: float
+
+    :return: adjusted value post calculation
+    :rtype: float
+    """
     if value is not None:
         return value / 1000  # Do not adjust missing/null responses
 
@@ -241,6 +350,18 @@ def adjust_value(value: Optional[float]) -> Optional[float]:
 def adjust_target_variables(
     do_adjustment: bool, target_variables: List[Target_variable]
 ) -> List[Target_variable]:
+    """
+    Method to amend the variables within the target_variables list and append the new values to the individual objects
+
+    :param do_adjustment: Determines if the values should be corrected
+    :type do_adjustment: bool
+    :param target_variables: list of Target_variable objects that can be corrected
+    :type target_variables: List[Target_variable]
+
+    :return: A returned list of target variables with the final_value updated with either the corrected value, or the
+    original value if no correction is to be made
+    :rtype: List[Target_variable]
+    """
     adjusted_target_variables = []
     for question in target_variables:
         if do_adjustment:
