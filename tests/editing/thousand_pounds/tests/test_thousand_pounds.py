f8042b20d755e4eee3bb257062f62eaba56360a7
jasonbellONS <jason.bell@ons.gov.uk>
2023-09-15 08:42:24 +0100

    running black, isort, flake8

--- tests/editing/thousand_pounds/tests/test_thousand_pounds.py
+++ tests/editing/thousand_pounds/tests/test_thousand_pounds.py
@@ -3,8 +3,8 @@ import pytest
 from sml_small.editing.thousand_pounds.thousand_pounds import (Target_variable, Thousands_output, TPException,
                                                                adjust_target_variables, adjust_value,
                                                                calculate_error_ratio, determine_predictive_value,
-                                                               determine_tpc_marker, is_within_threshold, thousand_pounds,
-                                                               validate_input)
+                                                               determine_tpc_marker, is_within_threshold,
+                                                               thousand_pounds, validate_input)
 from sml_small.utils.error_utils import (get_boundary_error, get_mandatory_param_error,
                                          get_one_of_params_mandatory_error, get_params_is_not_a_number_error)
 
