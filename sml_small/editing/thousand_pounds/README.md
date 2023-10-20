d3fb5a08f3122114ad0040e9c779c41ea5c26f6b
JasonBellONS <95040942+JasonBellONS@users.noreply.github.com>
2023-10-20 09:49:18 +0100

    Amend readmes (#63)
    * Readme amended, table formatting corrected
    
    * Amending thousand_pounds/example.py
    
    * Running black, flake8, isort
    
    * amending readme input format
    
    * Readme amended, table formatting corrected
    
    ---------
    
    Co-authored-by: James Morgan <89923453+jhmjam@users.noreply.github.com>

--- sml_small/editing/thousand_pounds/README.md
+++ sml_small/editing/thousand_pounds/README.md
@@ -7,8 +7,8 @@ An example of running the method directly and using csv data (in-memory and from
 Example input data: (2 csv files)
 
 ```
-principal_identifier,principal_variable,predictive,auxiliary,upper_limit,lower_limit
-12340000001-201409-q100,50000000,60000,30000,1350,350
+unique_identifier,principal_variable,predictive,auxiliary,upper_limit,lower_limit,precision
+12340000001-201409-q100,50000000,60000,30000,1350,350,2
 
 identifier,value
 q101,500
@@ -18,15 +18,16 @@ q104,
 ```
 
 i.e.
-|principal_identifier|principal_variable|predictive|auxiliary|upper_limit|lower_limit|
-|---|---|---|---|---|---|
-12340000001-201409-q100|50000000|60000|30000|1350|350
 
-|identifier|value|
-|-|-|
-q101|500
-q102|1000
-q103|1500
+|unique_identifier|principal_variable|predictive|auxiliary|upper_limit|lower_limit|precision
+|---|---|---|---|---|---|---|
+12340000001-201409-q100|50000000|60000|30000|1350|350|2
+
+|identifier| value 
+|---|----|
+q101| 500 
+q102| 1000 
+q103| 1500 
 q104|
 
 ## Calling the method
@@ -35,13 +36,15 @@ The method has the following interface:
 
 ```python
 def thousand_pounds(
-    principal_identifier: Optional[str],  # Unique identifer e.g. a question code/ruref/period/id/combination of all of thse
+    unique_identifier: Optional[str],  # Unique identifer e.g. a question code/ruref/period/id/combination of all of thse
     principal_variable: float,  # Original response value provided for the 'current' period
     predictive: Optional[float],  # Value used for 'previous' response (Returned/Imputed/Constructed)
     auxiliary: Optional[float],  # Calculated response for the 'previous' period
     upper_limit: float,  # Upper bound of 'error value' threshold
     lower_limit: float,  # Lower bound of 'error value' threshold
-    target_variables: List[Target_variable] # identifier/value pairs
+    target_variables: List[Target_variable], # identifier/value pairs
+    precision: Optional[int],  #Precision is used by the decimal package to ensure a specified accuracy
+    # used throughout method processing
 )
 ```
 
@@ -49,17 +52,17 @@ e.g.
 
 ```python
 output = thousand_pounds(
-    principal_identifier = "12340000001-201409-q100",
-    principal_variable = 50000000,
-    predictive = 60000,
-    auxiliary = 30000,
-    upper_limit = 1350,
-    lower_limit = 350,
-    target_variables = [
-            Target_variable(identifier="q101", original_value=500),
-            Target_variable(identifier="q102", original_value=1000),
-            Target_variable(identifier="q103", original_value=1500),
-            Target_variable(identifier="q104", original_value=None)]
+    unique_identifier = "12340000001-201409-q100",
+    principal_variable = "50000000",
+    predictive = "60000",
+    auxiliary = "30000",
+    upper_limit = "1350",
+    lower_limit = "350",
+    target_variables = {"q101": 500,
+                        "q102": 1000,
+                        "q103": 1500,
+                        "q104": None},
+    precision = 1
 )
 ```
 
@@ -68,14 +71,13 @@ output = thousand_pounds(
 ```python
 Thousands_output(
     principal_identifier='12340000001-201409-q100',
-    principal_original_value=50000000,
-    principal_adjusted_value=50000.0,
+    principal_adjusted_value="50000.0",
     target_variables=[
-        Target_variable(identifier='101', original_value=500, adjusted_value=0.5),
-        Target_variable(identifier='102', original_value=1000, adjusted_value=1.0),
-        Target_variable(identifier='103', original_value=1500, adjusted_value=1.5),
+        Target_variable(identifier='101', original_value="500", adjusted_value="0.5"),
+        Target_variable(identifier='102', original_value="1000", adjusted_value="1.0"),
+        Target_variable(identifier='103', original_value="1500", adjusted_value="1.5"),
         Target_variable(identifier='104', original_value=None, adjusted_value=None)],
-    tpc_ratio=833.3333333333334,
+    tpc_ratio="833.3",
     tpc_marker='C',
     error_description=''
 )
@@ -83,9 +85,9 @@ Thousands_output(
 
 Alternatively presented: (mixing input and output data for comparisons)
 
-|principal_identifier|principal_variable|predictive|aux|threshold_upper|threshold_lower|TPC_marker|ratio|principal_adjusted_value|target_variable|target_original_value|target_adjusted_value
-|---|---|---|---|---|---|---|---|---|---|---|---|
-12340000001-201409-q100|50000000|60000|15000|1350|350|C|1000.0|50000.0|q101|500|0.5
-12340000001-201409-q100|-|-|-|-|-|-|-|-|q102|1000|1
-12340000001-201409-q100|-|-|-|-|-|-|-|-|q103|1500|1.5
-12340000001-201409-q100|-|-|-|-|-|-|-|-|q104||
+| unique_identifier       |principal_variable|predictive|aux|threshold_upper|threshold_lower| tpc_marker | tpc_ratio |principal_adjusted_value|target_variable|target_adjusted_value
+|-------------------------|---|---|---|---|---|------------|-----------|---|---|---|
+ 12340000001-201409-q100 |50000000|60000|15000|1350|350| C          | 1000.0    |50000.0|q101|0.5
+ 12340000001-201409-q100 |-|-|-|-|-| -          | -         |-|q102|1
+ 12340000001-201409-q100 |-|-|-|-|-| -          | -         |-|q103|1.5
+ 12340000001-201409-q100 |-|-|-|-|-| -          | -         |-|q104||
--- sml_small/editing/thousand_pounds/example.py
+++ sml_small/editing/thousand_pounds/example.py
@@ -2,16 +2,18 @@ import dataclasses
 import json
 from csv import DictReader
 
-from thousand_pounds import Target_variable, thousand_pounds
+from thousand_pounds import thousand_pounds
 
 
 # Load some local csvs into memory and then run the pounds thousands method
 def invoke_process_with_local_csv():
     # Load the csvs into memory
-    with open("tests/config.csv") as file:
+    with open("../../../tests/editing/thousand_pounds/tests/config.csv") as file:
         config_csv = file.read()
 
-    with open("tests/linked_questions.csv") as file:
+    with open(
+            "../../../tests/editing/thousand_pounds/tests/linked_questions.csv"
+    ) as file:
         target_questions_csv = file.read()
 
     return invoke(config_csv, target_questions_csv)
@@ -46,17 +48,13 @@ def invoke(config_csv: str, linked_question_csv: str):
 
     # Format:  identifier, value
     linked_question_reader = DictReader(linked_question_csv.splitlines())
-    linked_questions = []
+    linked_questions = {}
     for linked_row in linked_question_reader:
         # print(f"Linked row: {linked_row}")
-        linked_questions.append(
-            Target_variable(
-                identifier=linked_row["identifier"],
-                original_value=None
-                if not linked_row["value"]
-                else float(linked_row["value"]),
-            )
-        )
+        if not linked_row["value"]:
+            linked_questions[linked_row["identifier"]] = "nan"
+        else:
+            linked_questions[linked_row["identifier"]] = linked_row["value"]
 
     return thousand_pounds(
         unique_identifier=config["unique_identifier"],
@@ -79,19 +77,16 @@ def invoke_directly_writing_output_to_csv_example():
         auxiliary=30000,
         upper_limit=1350,
         lower_limit=350,
-        target_variables=[
-            Target_variable(identifier="q101", original_value=500),
-            Target_variable(identifier="q102", original_value=1000),
-            Target_variable(identifier="q103", original_value=1500),
-            Target_variable(identifier="q104", original_value=None),
-        ],
+        target_variables={"q101": 500, "q102": 1000, "q103": 1500, "q104": None},
     )
     print(f"\nOutput: {output}")
-    print(f"\nOutput json: {json.dumps(dataclasses.asdict(output),indent=4)}")
+    print(f"\nOutput json: {json.dumps(dataclasses.asdict(output), indent=4)}")
 
     header = "unique_identifier,principal_original_value,principal_adjusted_value,tpc_ratio,tpc_marker,error_description"
-    row = f"{output.unique_identifier},{output.principal_original_value},{output.principal_final_value}," \
-          f"{output.tpc_ratio},{output.tpc_marker},{output.error_description}"
+    row = (
+        f"{output.unique_identifier},{output.principal_final_value},"
+        f"{output.tpc_ratio},{output.tpc_marker}"
+    )
 
     for linkedq in output.target_variables:
         header += (
@@ -128,14 +123,13 @@ def invoke_process_with_inmemory_single_csv_example():
     configs = []
     question_list = []
     for config_row in config_reader:
-        question_list = {key: v for key, v in config_row.items() if "q" in key}
-        linked_questions = []
+        question_list = {key: v for key, v in config_row.items() if "q1" in key}
+        linked_questions = {}
         for question in question_list:
-            linked_questions.append(
-                Target_variable(
-                    identifier=question, original_value=question_list[question]
-                )
-            )
+            if not question_list[question]:
+                linked_questions[question] = "nan"
+            else:
+                linked_questions[question] = question_list[question]
         config_row["target_variables"] = linked_questions
         configs.append(config_row)
 
@@ -147,21 +141,26 @@ def invoke_process_with_inmemory_single_csv_example():
     output_header = f"unique_identifier,principal_original_value,principal_final_value,tpc_ratio,tpc_marker,error_description{output_question_header}"
     output_row = ""
     for config in configs:
-        output = thousand_pounds(
-            unique_identifier=config["unique_identifier"],
-            principal_variable=config["principal_variable"],
-            predictive=config["predictive"],
-            auxiliary=config["auxiliary"],
-            upper_limit=config["upper_limit"],
-            lower_limit=config["lower_limit"],
-            target_variables=config["target_variables"],
-        )
-
-        output_row += f"{output.unique_identifier},{output.principal_original_value},{output.principal_final_value}," \
-                      f"{output.tpc_ratio},{output.tpc_marker},{output.error_description}"
-        for question in output.target_variables:
-            output_row += f",{question.original_value},{question.final_value}"
-        output_row += "\n"
+        try:
+            output = thousand_pounds(
+                unique_identifier=config["unique_identifier"],
+                principal_variable=config["principal_variable"],
+                predictive=config["predictive"],
+                auxiliary=config["auxiliary"],
+                upper_limit=config["upper_limit"],
+                lower_limit=config["lower_limit"],
+                target_variables=config["target_variables"],
+            )
+            output_row += (
+                f"{output.unique_identifier},{output.principal_final_value},"
+                f"{output.tpc_ratio},{output.tpc_marker}"
+            )
+            for question in output.target_variables:
+                output_row += f",{question.original_value},{question.final_value}"
+            output_row += "\n"
+        except Exception as error:
+            output_row += f"\nerror in row {config['unique_identifier']}"
+            output_row += f"\n {error}"
 
     print("\nSingle_output csv:")
     print(f"{output_header}")
