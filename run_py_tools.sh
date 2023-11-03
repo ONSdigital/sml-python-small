4198c577e25285b88c601e16dcc94c0ebedea88b
Joseph-Percival-ONS <joseph.percival@ons.gov.uk>
2023-11-03 16:36:52 +0000

    add return value reflecting test success

--- run_py_tools.sh
+++ run_py_tools.sh
@@ -6,6 +6,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'  # No color
 results=()
+return_val=0
 for i in "${CMDS[@]}"
 do 
     if $i
@@ -13,6 +14,7 @@ do
         results+=("$(printf "%-40s" "$i"): ${GREEN}Success${NC}")
         echo "$i command successfully executed"
     else
+        return_val=1
         results+=("$(printf "%-40s" "$i"): ${RED}Failure${NC}")
         echo "$i command failed to execute or displays errors that needs to be corrected"
     fi
@@ -20,6 +22,7 @@ done
 
 cd tests
 pytest
+return_val=$((return_val || $?)) 
 cd ..
 
 echo "Test Results:"
@@ -27,3 +30,5 @@ for result in "${results[@]}"
 do
     echo "$result"
 done
+
+exit "$return_val"
