0da1c55b6d92219ad3b96110059b799ca9d00999
Steve Gibbard <steve.gibbard@ons.gov.uk>
2023-11-06 16:21:04 +0000

    Update import order

--- sml_small/utils/pandas_example.py
+++ sml_small/utils/pandas_example.py
@@ -4,10 +4,10 @@ currently this includes Totals and Components, and Thousand Pounds Correction
 
 For Copyright information, please see LICENCE.
 """
+import os
 import re
 
 import pandas as pd
-import os
 
 # We import the wrapper function from the pandas_wrapper
 from sml_small.utils.pandas_wrapper import wrapper
