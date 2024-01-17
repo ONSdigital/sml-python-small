658488e33534680902163d60b132c60da026733c
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-01-17 14:10:17 +0000

    TPC method CSV files testing integrated

--- tests/editing/uat-testing/uat_testing.py
+++ tests/editing/uat-testing/uat_testing.py
@@ -30,10 +30,10 @@ def run_all_csvs(directory, function):
                     directory, input_filename, output_filename
                 )
 
-            # elif function == "thousand_pounds":
-            #     run_thousand_pounds_with_pandas(
-            #         directory, input_filename, output_filename
-            #     )
+            elif function == "thousand_pounds":
+                run_thousand_pounds_with_pandas(
+                    directory, input_filename, output_filename
+                )
 
 
 # Function below is used to read a CSV file from the given
@@ -147,6 +147,67 @@ def run_totals_components_with_pandas(path, input_csv, output_csv):
     final_df.to_csv(csv_filename, index=False)
 
 
+# ---------------------------------------
+# Thousand Pounds usage with Pandas
+# ---------------------------------------
+def run_thousand_pounds_with_pandas(path, input_csv, output_csv):
+    input_dataframe_thousand_pounds = load_csv(path + input_csv)
+
+    # match target variable columns q<number>
+    list_column_pattern = r"q\d+"
+    target_variables = filter_columns_by_pattern(
+        input_dataframe_thousand_pounds, list_column_pattern
+    )
+
+    thousand_pounds_output_columns = [
+        "principal_final_value",
+        "target_variables",
+        "tpc_ratio",
+        "tpc_marker",
+    ]
+    test_thousand_pounds = wrapper(
+        input_dataframe_thousand_pounds,
+        "thousand_pounds",
+        output_columns=thousand_pounds_output_columns,
+        unique_identifier_column="RU",
+        principal_variable_column="principal_val",
+        target_variables_columns=target_variables,
+        upper_limit_column="threshold_upper",
+        lower_limit_column="threshold_lower",
+        predictive_column="predictive_val",
+        auxiliary_column="aux_val",
+    )
+
+    # Expand columns that contain a list of objects (e.g target_variable [TargetVariable(identifier='q42', original_value='32', final_value='0.032'),...])
+    # and create separate columns e.g: q42_final_value
+    final_data = expand_list_column(
+        df=test_thousand_pounds,
+        list_column_name="target_variables",
+        prefix_attribute="identifier",
+    )
+
+    # Create a new DataFrame from the final_data list
+    final_df = pd.DataFrame(final_data)
+
+    # Drop the "target_variables" column
+    final_df.drop(columns=["target_variables"], inplace=True)
+
+    # Move the tpc_marker to be the last column
+    column_to_move = "tpc_marker"
+
+    # Define the desired column order with the specified column at the end
+    column_order = [col for col in final_df.columns if col != column_to_move] + [
+        column_to_move
+    ]
+
+    # Update the DataFrame with the columns in the desired order
+    final_df = final_df[column_order]
+
+    # Write the DataFrame to a CSV, excluding the index column
+    csv_filename = "tpc_test_data_processed/" + output_csv
+    final_df.to_csv(csv_filename, index=False)
+
+
 # Run example data for Totals and Components
 # run_totals_components_with_pandas(
 #     "../../tests/editing/totals_and_components/example_data/",
@@ -164,44 +225,14 @@ run_all_csvs("tcc_test_data_original/", "totals_and_components")
 #     "example_test_data_pandas_output.csv",
 # )
 
-# # Run Thousand Pounds UAT
-# run_all_csvs("../../tests/editing/thousand_pounds/example_data/uat/", "thousand_pounds")
+# Run Thousand Pounds UAT
+# run_all_csvs("tpc_test_data_original/", "thousand_pounds")
 
 
-
-
-# UAT Testing
-fill_value = 'default_value'
-
-# @pytest.fixture(scope="module")
-# @dt.working_directory(__file__)
-# def df_input():
-#     return pd.read_csv("TCC_test_data/TCC_test_data_case_a1.csv")
-
-
-# @pytest.fixture(scope="module")
-# @dt.working_directory(__file__)
-# def df_temp_output():
-#     df = pd.read_csv("TCC_test_output_data/TCC_test_data_case_a1_temp_output.csv")
-#     df = df.fillna(fill_value)
-
-#     return df
-
-# @pytest.fixture(scope="module")
-# @dt.working_directory(__file__)
-# def df_correct_output():
-#     df = pd.read_csv("TCC_test_data/TCC_test_data_case_a1_output.csv")
-#     df = df.drop(columns=['period'])
-#     df = df.fillna(fill_value)
-#     df.loc[df["TCC_marker"] == "N", "TCC_marker"] = fill_value
-#     column_to_move = df.pop("TCC_marker")
-#     df.insert(19, "TCC_marker", column_to_move)
-
-#     return df
-
-
+# UAT CSV files are used to run with example Pandas code for Totals and Components and Thousand Pounds Correction
+# Output generated is checked against the expected output files
 @pytest.mark.mandatory
-def test_input_columns():
+def test_input_columns_tcc():
     for filename in os.listdir("tcc_test_data_original/"):
         if filename.endswith(".csv") and "output" not in filename:
             # print(filename)
@@ -226,37 +257,7 @@ def test_input_columns():
 
 
 @pytest.mark.mandatory
-def test_output_columns():
-    for filename in os.listdir("tcc_test_data_processed/"):
-        df_processed_output = pd.read_csv("tcc_test_data_processed/" + filename)
-        dt.validate(
-            df_processed_output.columns,
-            {
-                "reference",
-                "total",
-                "comp_1",
-                "comp_2",
-                "comp_3",
-                "comp_4",
-                "amend_total",
-                "predictive",
-                "auxiliary",
-                "abs_threshold",
-                "perc_threshold",
-                "abs_diff",
-                "perc_low",
-                "perc_high",
-                "tcc_marker",
-                "final_total",
-                "final_component_1",
-                "final_component_2",
-                "final_component_3",
-                "final_component_4"
-            }
-        )
-
-@pytest.mark.mandatory
-def test_values():
+def test_values_tcc():
     tcc_test_data_original = os.listdir("tcc_test_data_original/")
     tcc_test_data_processed = os.listdir("tcc_test_data_processed/")
 
@@ -295,3 +296,126 @@ def test_values():
                         print(f"Correct output: {df_correct_output.iloc[row, col]}")
                         print(f"Processed output: {df_processed_output.iloc[row, col]}")
                         dt.validate.superset(df_processed_output, df_correct_output)
+
+
+@pytest.mark.mandatory
+def test_output_columns_tcc():
+    for filename in os.listdir("tcc_test_data_processed/"):
+        df_processed_output = pd.read_csv("tcc_test_data_processed/" + filename)
+        dt.validate(
+            df_processed_output.columns,
+            {
+                "reference",
+                "total",
+                "comp_1",
+                "comp_2",
+                "comp_3",
+                "comp_4",
+                "amend_total",
+                "predictive",
+                "auxiliary",
+                "abs_threshold",
+                "perc_threshold",
+                "abs_diff",
+                "perc_low",
+                "perc_high",
+                "tcc_marker",
+                "final_total",
+                "final_component_1",
+                "final_component_2",
+                "final_component_3",
+                "final_component_4"
+            }
+        )
+
+
+@pytest.mark.mandatory
+def test_input_columns_tpc():
+    for filename in os.listdir("tpc_test_data_original/"):
+        if filename.endswith(".csv") and "output" not in filename:
+            print(filename)
+            df_input = pd.read_csv("tpc_test_data_original/" + filename)
+
+            ignore_columns = ["q42", "q43"]
+            for col in ignore_columns:
+                if col in df_input.columns:
+                    df_input = df_input.drop(columns=ignore_columns) 
+
+            dt.validate(
+                df_input.columns,
+                {
+                    "RU",
+                    "period",
+                    "principal_val",
+                    "predictive_val",
+                    "aux_val",
+                    "threshold_upper",
+                    "threshold_lower"
+                }
+            )
+
+
+@pytest.mark.mandatory
+def test_values_tpc():
+    tcc_test_data_original = os.listdir("tpc_test_data_original/")
+    tcc_test_data_processed = os.listdir("tpc_test_data_processed/")
+
+    for file1 in tcc_test_data_processed:
+        for file2 in tcc_test_data_original:
+            if file1 == file2:
+                print("\n")
+                print("====================================================================================================================")
+                print(f"Filename '{file1}' and '{file2}' is present in both directories.")
+                df_processed_output = pd.read_csv("tpc_test_data_processed/" + file1)
+
+                df_correct_output = pd.read_csv("tpc_test_data_original/" + file2)
+
+                df_processed_output = df_processed_output.fillna(0)
+                df_correct_output = df_correct_output.fillna(0)
+
+                # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+                #     print("\n")
+                #     print("Correct output")
+                #     print(df_correct_output)
+                #     print("\n")
+                #     print("Processed output")
+                #     print(df_processed_output)
+
+                # dt.validate.superset(df_processed_output, df_correct_output)
+
+                # Compare the dataframes
+                comparison = df_processed_output == df_correct_output
+                if comparison.all().all():
+                    print("CSV files match.")
+                    dt.validate.superset(df_processed_output, df_correct_output)
+                else:
+                    mismatch_locations = np.where(comparison == False)
+                    for row, col in zip(*mismatch_locations):
+                        print(f"Value mismatch at row {row}, column {col}.")
+                        print(f"Correct output: {df_correct_output.iloc[row, col]}")
+                        print(f"Processed output: {df_processed_output.iloc[row, col]}")
+                        dt.validate.superset(df_processed_output, df_correct_output)
+
+@pytest.mark.mandatory
+def test_output_columns_tpc():
+    for filename in os.listdir("tpc_test_data_processed/"):
+        df_processed_output = pd.read_csv("tpc_test_data_processed/" + filename)
+        dt.validate(
+            df_processed_output.columns,
+            {
+                "RU",
+                "period",
+                "principal_val",
+                "q42",
+                "q43",
+                "predictive_val",
+                "aux_val",
+                "threshold_upper",
+                "threshold_lower",
+                "tpc_ratio",
+                "final_principal",
+                "final_q42",
+                "final_q43",
+                "tpc_marker"
+            }
+        )
